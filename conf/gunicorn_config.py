cff43c400d393eaddd548793dd43066d71de867c
LionHeart-coder <korsakovsergey07062000@gmail.com>
2021-01-24 12:45:38 +0300

    settings for deploy

---
+++ conf/gunicorn_config.py
@@ -0,0 +1,4 @@
+command = '/home/sergey/.virtualenvs/venv_for_projects/hw05_final/bin/gunicorn'
+pythonpath = '/home/sergey/.virtualenvs/venv_for_projects/hw05_final'
+bind = 'localhost:8000'
+workers = 2
