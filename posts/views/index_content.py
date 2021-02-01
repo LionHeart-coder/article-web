a069ef4ccd744b49813c8023af322fcf6b0e076c
LionHeart-coder <korsakovsergey07062000@gmail.com>
2021-02-01 19:29:56 +0300

    Added best authors on index page

--- posts/views/index_content.py
+++ posts/views/index_content.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.db.models import Count
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.http import HttpResponseBadRequest
 from django.shortcuts import get_object_or_404, render, render_to_response
 
 from posts import posts_settings
@@ -11,9 +11,9 @@ from posts.models import Post, User
 def index(request):
     post_list = (
         Post.objects.all()
-            .select_related('group', 'author')
-            .prefetch_related('comments', 'likes')
-            .annotate_like(request.user)
+        .select_related('group', 'author')
+        .prefetch_related('comments', 'likes')
+        .annotate_like(request.user)
     )
 
     paginator = Paginator(post_list, posts_settings.INDEX_NUMBER_OF_POSTS)
@@ -27,10 +27,11 @@ def index(request):
 
 def best_articles(request):
     post_list = (
-        Post.objects.annotate(rating=Count('likes') + Count('comments')).order_by('-rating')[:5]
-            .select_related('group', 'author')
-            .prefetch_related('comments', 'likes')
-            .annotate_like(request.user)
+        Post.objects.annotate(rating=Count('likes') + Count('comments'))
+        .order_by('-rating')[:5]
+        .select_related('group', 'author')
+        .prefetch_related('comments', 'likes')
+        .annotate_like(request.user)
     )
 
     paginator = Paginator(post_list, posts_settings.INDEX_NUMBER_OF_POSTS)
@@ -38,14 +39,21 @@ def best_articles(request):
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
     context = {
-        'page': page, 'paginator': paginator, 'request': request,
+        'page': page,
+        'paginator': paginator,
+        'request': request,
     }
-    return render_to_response("posts/includes/index_content.html", context)
+    return render_to_response('posts/includes/index_content.html', context)
 
 
 def best_authors(request):
-    # Список лучший авторов с их достижениями
-    return JsonResponse({"status": "ok"})
+    author_list = User.objects.annotate(
+        rating=Count('posts__comments', distinct=True)
+        + Count('posts__likes', distinct=True)
+        + Count('following')
+    ).order_by('-rating')[:6]
+    context = {'author_list': author_list}
+    return render_to_response('posts/includes/best_authors.html', context)
 
 
 @login_required
@@ -54,9 +62,9 @@ def favorites_authors(request, username):
     if request.user == user:
         posts_list = (
             Post.objects.filter(author__following__user=request.user)
-                .select_related('group', 'author')
-                .prefetch_related('comments')
-                .annotate_like(request.user)
+            .select_related('group', 'author')
+            .prefetch_related('comments')
+            .annotate_like(request.user)
         )
 
         paginator = Paginator(
@@ -69,5 +77,5 @@ def favorites_authors(request, username):
             'page': page,
             'paginator': paginator,
         }
-        return render_to_response("posts/includes/index_content.html", context)
+        return render_to_response('posts/includes/index_content.html', context)
     return HttpResponseBadRequest()
