5c534f1f7e6ffb64a8bf929d2f15bfcb5435ef16
LionHeart-coder <korsakovsergey07062000@gmail.com>
2021-02-14 19:08:34 +0300

    Production settings for Docker and nginx

--- conf/gunicorn_config.py
+++
@@ -1,4 +0,0 @@
-command = '/home/sergey/.virtualenvs/venv_for_projects/hw05_final/bin/gunicorn'
-pythonpath = '/home/sergey/.virtualenvs/venv_for_projects/hw05_final'
-bind = 'localhost:8000'
-workers = 2
